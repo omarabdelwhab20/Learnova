425a99bddeef1eb5ddb74e16c3bed6bb79204057
Omar Abd Elwahab <omarabdelwhab20@gmail.com>
2026-08-22 18:31:27 +0300

    feat: installed the needed packages for now

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (confgiService: ConfigService) => ({
+        type: 'postgres',
+        port: Number(confgiService.get('PG_PORT')),
+        host: confgiService.get('PG_HOST'),
+        username: confgiService.get('PG_USER'),
+        password: confgiService.get('PG_PASS'),
+        database: confgiService.get('PG_DBNAME'),
+        synchronize: true,
+      }),
+    }),
+
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60_000,
+        limit: 10,
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
